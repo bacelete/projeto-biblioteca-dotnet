3347dcd3f6801d83eacc32d8e38ded321a156989
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-12-05 13:15:03 -0300

    feat: classe do usuario

---
+++ BibliotecaApp/App/Domain/Entities/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public List<Emprestimo> EmprestimosAtivos { get; set; }
+
+    }
+}
