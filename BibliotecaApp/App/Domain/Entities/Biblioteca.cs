112045ae02b4a6ca5dde5a54564bdd7b75174f3c
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-12-05 11:20:00 -0300

    feat: escopo inicial com as classes (biblioteca e livro)

---
+++ BibliotecaApp/App/Domain/Entities/Biblioteca.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Domain.Entities
+{
+    public class Biblioteca
+    {
+    }
+}
---
+++ BibliotecaApp/App/Domain/Entities/Livro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Domain.Entities
+{
+    public class Livro
+    {
+        public String Titulo { get; set; }
+        public String Autor { get; set; }
+        public String ISBN { get; set; }
+        public DateOnly AnoPublicacao { get; set; }
+        public enum Categoria
+        {
+            Ficcao,
+            Romance,
+            Exatas,
+            Historia,
+            Biografia,
+            Tecnologia
+        }
+        public enum Status
+        {
+            Disponivel,
+            Emprestado,
+            Reservado
+        }
+
+    }
+}
