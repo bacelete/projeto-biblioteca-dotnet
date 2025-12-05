f08432eec70368f78263b41b2de5c3ca3ac6f813
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-12-05 16:13:50 -0300

    feat: metodo de emprestar livro

--- BibliotecaApp/App/Domain/Entities/Emprestimo.cs
+++ BibliotecaApp/App/Domain/Entities/Emprestimo.cs
@@ -6,11 +6,27 @@ namespace App.Domain.Entities
 {
     public class Emprestimo
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public int IdLivro { get; set; }
         public int IdUsuario { get; set; }
         public DateOnly DataEmprestimo { get; set; }
         public DateOnly DataDevolucao { get; set; }
 
+        public Emprestimo(Guid id, int idLivro, int idUsuario, DateOnly dataEmprestimo, DateOnly dataDevolucao)
+        {
+            Id = id;
+            IdLivro = idLivro;
+            IdUsuario = idUsuario;
+            DataEmprestimo = dataEmprestimo;
+            DataDevolucao = dataDevolucao;
+        }
+
+        public Emprestimo(Guid id, int idLivro, int idUsuario, DateOnly dataEmprestimo)
+        {
+            Id = id;
+            IdLivro = idLivro;
+            IdUsuario = idUsuario;
+            DataEmprestimo = dataEmprestimo; 
+        }
     }
 }
--- BibliotecaApp/App/Domain/Services/EmprestimoService.cs
+++ BibliotecaApp/App/Domain/Services/EmprestimoService.cs
@@ -7,9 +7,11 @@ namespace App.Domain.Services
 {
     public class EmprestimoService
     {
-        public void EmprestarLivro(Usuario usuario)
+        public void EmprestarLivro(Livro livro, Usuario usuario)
         {
-
+            Emprestimo emprestimo = new Emprestimo(Guid.NewGuid(), livro.Id, usuario.Id, DateOnly.Parse("2025-12-05"));
+            usuario.EmprestimosAtivos.Add(emprestimo);    
         }
+
     }
 }
