df85a2ec09b0f618b9bf24b4cf27675aabdcae4c
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-12-05 11:27:53 -0300

    feat: adicionando classe emprestimo + atributos

---
+++ BibliotecaApp/App/Domain/Entities/Emprestimo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Domain.Entities
+{
+    public class Emprestimo
+    {
+        public int Id { get; set; }
+        public int IdLivro { get; set; }
+        public int IdUsuario { get; set; }
+        public DateOnly DataEmprestimo { get; set; }
+        public DateOnly DataDevolucao { get; set; }
+
+    }
+}
--- BibliotecaApp/App/obj/Debug/net10.0/App.AssemblyInfo.cs
+++ BibliotecaApp/App/obj/Debug/net10.0/App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+112045ae02b4a6ca5dde5a54564bdd7b75174f3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
